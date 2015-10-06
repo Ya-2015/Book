67f821585cbd010ccb218c987098ff9397bf9838
Ya-2015 <ya.liu@infosys.com>
2015-10-06 10:40:01 -0400

    BookDB

---
+++ Book/src/BookDB/Book.java
@@ -0,0 +1,68 @@
+package BookDB;
+
+public class Book {
+	
+	private String title = "";
+	private String author = "";
+	private String description = "";
+	private double price = 0;
+	private boolean isInStock = false;
+	
+	public Book(String title, String author, String description, double price, boolean isInStock){
+		this.title = title;
+		this.author = author;
+		this.description = description;
+		this.price = price;
+		this.isInStock = isInStock;
+	}
+	
+	public void setTitle(String title){
+		this.title = title;
+	}
+	
+	public String getTitle(){
+		return this.title;
+	}
+	
+	public void setAuthor(String author){
+		this.author = author;
+	}
+	
+	public String getAuthor(){
+		return this.author;
+	}
+	
+	public void setDescription(String des){
+		this.description = des;
+	}
+	
+	public String getDescription(){
+		return this.description;
+	}
+	
+	public void setPrice(double price){
+		this.price = price;
+	}
+	
+	public double getPrice(){
+		return this.price;
+	}
+	
+	public void setIsInStock(boolean isInStock){
+		this.isInStock = isInStock;
+	}
+	
+	public boolean getIsInStock(){
+		return this.isInStock;
+	}
+	
+	public double pricing(double numberOfBooks){
+		if (this.isInStock){
+			return numberOfBooks * price;
+		}else{
+			return 0;
+		}
+		
+	}
+	
+}
---
+++ Book/src/BookDB/BookDB.java
@@ -0,0 +1,22 @@
+package BookDB;
+
+import java.util.Scanner;
+
+import MyPkg.Book;
+
+public class BookDB {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		Book book = new Book("Pride and Prejudice", "Jane Austen", "One of the most universally loved and admired English novels", 9.99, true);
+		System.out.println("Book Info");
+		System.out.println("\tTitle:\t\t" + book.getTitle());
+		System.out.println("\tAuthor:\t\t" + book.getAuthor());
+		System.out.println("\tDescription:\t" + book.getDescription() + "\n");
+		System.out.print("How many books you request?\t");
+		int num = scan.nextInt();
+		System.out.println("\nBook Instock Status: " + book.getIsInStock() + "\tPricing: " + book.pricing(num));
+		
+		scan.close();
+	}
+}
